2aecb0516b32d40de72ee0814898813023e265d8
maciej.wolny <maciej.wolny@openx.com>
2021-03-29 17:08:01 +0200

    deployment fixes

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ CORS(app)
 FlaskInjector(app=app, modules=[configure])
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True, threaded=True)
+    app.run(host="127.0.0.1", port=8080, debug=True, threaded=True)
